04c03f029b475f01ec459f4104d927997f02ec51
Daniele Angileri <Daniele Angileri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-17 15:03:33 +0000

    Changed CSS style of links without images to prevent whitespace Link to gallery instead of first image of gallery Skip videos until video-player is implemented
    git-svn-id: https://plugins.svn.wordpress.org/facebook-social-stream/tags/1.2.3@1201064 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/FBSS_CSS.php
+++ lib/FBSS_CSS.php
@@ -7,7 +7,7 @@ class FBSS_CSS {
 	
 	private static $logger;
 	private static $plugin_dir_url;
-	private static $version = '1.0.1';
+	private static $version = '1.0.2';
 	
 	public static function register() {
 		self::$logger = new FBSS_Logger(__CLASS__);
